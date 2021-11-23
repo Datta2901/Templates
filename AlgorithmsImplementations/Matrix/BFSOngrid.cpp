a515c42a1f24f23a140fea9e0374bcdf96af70c2
manikanta2901 <manikanta2901@gmail.com>
2021-11-23 16:09:49 +0530

    Bfs on grid sample problem added

--- AlgorithmsImplementations/Matrix/BFSOngrid.cpp
+++ AlgorithmsImplementations/Matrix/BFSOngrid.cpp
@@ -1,77 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
-private:
-    int m,n;
-public: 
-
-    bool isValid(int x,int y){
-        if(x < 0 || x >= m || y < 0 || y >= n){
-            return false;
-        }
-        return true;
-    }
-
-    void bfs(vector<vector<int> >&grid,vector<vector<bool> > &visited,vector<vector<int> >&distance){
-        queue<pair<int,int>> track;
-        track.push({0,0});
-        int dx[2] = {1,0};
-        int dy[2] = {0,1};
-        visited[0][0] = true;
-        distance[0][0] = grid[0][0];
-        while(!track.empty()){
-            int x = track.front().first;
-            int y = track.front().second;
-            track.pop();
-            for(int i = 0; i < 2; i++){
-                int X = x + dx[i];
-                int Y = y + dy[i];
-                if(isValid(X,Y) == false){
-                    continue;
-                }if(visited[X][Y] == true){
-                    distance[X][Y] = min(distance[X][Y],distance[x][y] + grid[X][Y]);
-                    continue;
+class Solution{
+    private:
+        int maximum = 0;
+    public:
+        void bfs(vector<vector<int> >& grid,int i,int j){
+            int count = 0;
+            int row = grid.size();
+            int col = grid[0].size();
+            queue<pair<int,int> >track;
+            track.push({i,j});
+            grid[i][j] = 2;
+            int dx[4] = {0,0,1,-1};
+            int dy[4] = {1,-1,0,0};
+            while(!track.empty()){
+                int X = track.front().first;
+                int Y = track.front().second;
+                track.pop();
+                for(int i = 0; i < 4; i++){
+                    int x = X + dx[i];
+                    int y = Y + dy[i];
+                    if((x < 0 || x >= row) || (y < 0 ||y >= col) ||  grid[x][y] == 0 || grid[x][y] == 2){
+                        continue;
+                    } 
+                    grid[x][y] = 2;
+                    count++;
+                    track.push({x,y});
                 }
-                visited[X][Y] = true;
-                track.push({X,Y});
-                distance[X][Y] = min(distance[X][Y],distance[x][y] + grid[X][Y]);
             }
+            maximum = max(maximum,count);
         }
-    }
 
-    int minPathSum(vector<vector<int> >& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        // cout << m << " " << n << endl;
-        vector<vector<bool> > visited(m,vector<bool>(n,false));
-        vector<vector<int> > distance(m,vector<int>(n,INT_MAX));
-        bfs(grid,visited,distance);
-        // cout << "Distance ... " << endl;
-        // for(auto it : distance){
-        //     for(auto n : it){
-        //         cout << n << " ";
-        //     }
-        //     cout << endl;
-        // }
-        return distance[m - 1][n - 1];
-    }
+        int maxAreaOfIsland(vector<vector<int> >& grid) {
+            int row = grid.size();
+            int col = grid[0].size();
+            int maxi = INT_MIN;
+            for(int i = 0; i < row; i++){
+                for(int j = 0; j < col; j++){
+                    if(grid[i][j] != 0){
+                        bfs(grid,i,j);
+                    }
+                }
+            }
+            return maximum;
+        }
 };
+
 
 int main(){
     Solution obj;
-    int m,n;
-    cin >> m >> n;
-    vector<vector<int> > grid(m);
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            int a;
-            cin >> a;
-            grid[i].push_back(a);
-        }
+    vector<int>num;
+    int size;
+    cin >> size;
+    for(int i = 0; i < size; i++){
+        int a;
+        cin >> a;
+        num.push_back(a);
     }
-
-    cout << obj.minPathSum(grid);
-    
     return 0;
 }
