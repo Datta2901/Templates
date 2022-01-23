2de8bdb00897b8e063e28bbcff147df15f20ca97
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-01-23 16:03:52 +0530

    Update DisjointSet.cpp

--- DataStructureTemplates/DisjointSet.cpp
+++ DataStructureTemplates/DisjointSet.cpp
@@ -1,57 +1,75 @@
+// disjointset union by size
 #include<bits/stdc++.h>
 using namespace std;
 
-void makeSet(int vertices,vector<int>&parent){
-    for(int i = 0; i < vertices; i++){
-        parent[i] = i; 
-    }
-}
-
-int findParent(int node,vector<int>&parent){
-    if(node == parent[node]){
-        return node;
-    }
-    return parent[node] = findParent(parent[node],parent);
-}
-
-void Union(int u,int v,vector<int>&parent,vector<int>&rank){
-    u = findParent(u,parent);
-    v = findParent(v,parent);
-    if(rank[u] < rank[v]){
-        parent[u] = v;
-    }else if(rank[v] < rank[u]){
-        parent[v] = u;
-    }else{
-        parent[u] = v;
-        rank[u]++;
-    }
-}
-
-int main(){
-    int vertices,edges;
-    cin >> vertices >> edges;
-    vector<int>rank(vertices,0);
-    vector<int>parent(vertices,0);
-    makeSet(vertices,parent);
-    for(int i = 0; i < edges; i++){
-        int u,v;
-        cin >> u >> v;
-        Union(u,v,parent,rank);
-    }
-    int queries;
-    cin >> queries;
-    while(queries--){
-        int type,u,v;
-        cin >> type >> u >> v;
-        if(type == 1){
-            Union(u,v,parent,rank);
-        }else{
-            if(findParent(u,parent) != findParent(v,parent)){
-                cout << "Not belongs to the same component" << endl;
-            }else{
-                cout << "Belongs to the same component" << endl;
+class DSU{
+    private:
+        vector<int>parent,size;
+    public:
+        DSU(int n){
+            for(int i = 0; i < n; i++){
+                parent.push_back(i);
+                size.push_back(1);
             }
         }
-    }
-    return 0;
+        void Union(int u,int v){
+            int parU = findParent(u);
+            int parV = findParent(v);
+            if(parU == parV){
+                return ;
+            }
+
+            if(size[parU] < size[parV]){
+                parent[parU] = parV;
+                size[parV] += size[parU];
+            }else{
+                parent[parV] = parU;
+                size[parU] += size[parV];
+            }
+        }
+
+        int findParent(int node){
+            if(parent[node] == node){
+                return node;
+            }
+            return parent[node] = findParent(parent[node]);
+        }
+};
+
+// disjointset union by Rank
+
+class DSURank{
+    private:
+        vector<int>parent,rank;
+    public:
+        DSURank(int n){
+            for(int i = 0; i < n; i++){
+                parent.push_back(i);
+                rank.push_back(1);
+            }
+        }
+        void Union(int u,int v){
+            int parU = findParent(u);
+            int parV = findParent(v);
+            if(parU == parV){
+                return ;
+            }
+            if(rank[parU] == rank[parV]){
+                parent[parV] = parU;
+                rank[parV]++;
+            }
+            if(rank[parU] == rank[parV]){
+                parent[parU] = parV; 
+            }else{
+                parent[parV] = parU;
+                rank[parU] += 1;
+            }
+        }
+
+        int findParent(int node){
+            if(parent[node] == node){
+                return node;
+            }
+            return parent[node] = findParent(parent[node]);
+        }
 }
